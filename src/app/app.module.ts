e1dbce29c6444b440a277d0552efaf23fc87d6be
vikash chauhan <vikashchauhanca@gmail.com>
2023-12-17 00:21:29 +0530

    ui update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AppComponent } from './app.component';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import { LoginComponent } from './login/login.component';
 import { AuthGuard } from './services/auth/auth.guard';
+import {  } from 'ngx-toastr';
 
 @NgModule({
   imports: [
@@ -25,6 +26,8 @@ import { AuthGuard } from './services/auth/auth.guard';
     AppRoutingModule,
     NgbModule,
     ToastrModule.forRoot(),
+    ToastrModule
+    
   ],
   declarations: [	
     AppComponent,
